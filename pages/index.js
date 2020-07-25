f80ddfc76e57b97782de40341e368ec45715ce1e
juicecultus <juicecultus@gmail.com>
2020-07-25 16:19:39 +0100

    add timer countdown and amend styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Timer from '../components/Timer';
 
 export default function Home() {
   return (
@@ -7,30 +8,47 @@ export default function Home() {
         <title>Rated Counsel App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <header>
+        <div className='logo'>
+          <svg width='128' height='128' viewBox='0 0 83.5 84'>
+            <path
+              class='st0'
+              d='M4.1,41.8C4,21.8,19.5,5.2,39.5,4.1v-4C16.5,1.4-1.1,20.9,0.1,43.9c1.1,21.3,18.1,38.3,39.4,39.4v-4.1
+		C19.6,78.2,4,61.7,4.1,41.8z'
+            />
+            <path
+              class='st0'
+              d='M35.6,44.4c5.7,0.8,11.2,3.1,15.6,6.8l1-2.2c-4.5-3.6-9.8-6.1-15.4-7.1L35.6,44.4z'
+            />
+            <path
+              class='st0'
+              d='M23.5,71.7c4.6,3.5,10.2,5.6,16,6l1-2.2c-5.8-0.4-11.3-2.5-16-6L23.5,71.7z'
+            />
+            <path
+              class='st0'
+              d='M75.1,66.7c5.4-7.2,8.3-16,8.3-25c0-22.1-17.2-40.3-39.3-41.6V4c20.8,1.4,36.6,19.3,35.2,40.1
+		c-0.5,7.7-3.4,15-8.2,21.1l-0.8,1l4.5,9.1L46.4,62l3.6-8.1c-2.3-3.4-13.3-8.9-14.5-9.5l-9.9,22.4c2.5,3.5,14.9,8.6,14.9,8.6
+		l4.3-9.8l38.7,18.1L75.1,66.7z'
+            />
+          </svg>
+        </div>
+      </header>
 
       <main>
-        <h1 className='title'>
-          <a>Welcome to Rated Counsel!</a>
-        </h1>
+        <h1 className='title'>Rated Counsel</h1>
 
-        <p className='description'>
-          The future of legal services is about to begin...
-        </p>
-
-        <h2 className='subtitle'>
-          <a>Watch this space</a>
-        </h2>
+        <Timer />
 
         <h2>Follow us on social</h2>
         <div className='wrapper'>
           <a href='https://www.facebook.com/RatedCounsel' target='_blank'>
-            <i className='fa fa-5x fa-facebook-square'></i>
+            <i className='fa fa-3x fa-facebook-square'></i>
           </a>
           <a href='https://twitter.com/ratedcounsel' target='_blank'>
-            <i className='fa fa-5x fa-twitter-square'></i>
+            <i className='fa fa-3x fa-twitter-square'></i>
           </a>
           <a href='https://www.instagram.com/ratedcounsel/' target='_blank'>
-            <i className='fa fa-5x fa-instagram'></i>
+            <i className='fa fa-3x fa-instagram'></i>
           </a>
         </div>
       </main>
@@ -49,13 +67,24 @@ export default function Home() {
           align-items: center;
         }
 
+        header {
+          margin-top: 2rem;
+        }
+
         main {
-          padding: 5rem 0;
+          padding: 0.5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+        }
+
+        .logo {
+        }
+
+        svg {
+          fill: #4867aa;
         }
 
         footer {
@@ -65,16 +94,8 @@ export default function Home() {
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+          color: #4867aa;
+          font-size: 0.7rem;
         }
 
         a {
@@ -94,20 +115,11 @@ export default function Home() {
         }
 
         .title {
-          margin: 0;
+          margin-top: 0;
+          margin-bottom: 2rem;
           line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .subtitle {
-          margin: 0 0 2rem 0;
-          line-height: 1.15;
-          font-size: 2rem;
-        }
-
-        .subtitle a {
+          font-size: 6vmax;
           color: #0070f3;
-          text-decoration: none;
         }
 
         .title,
@@ -115,22 +127,8 @@ export default function Home() {
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
         h2 {
           color: green;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
 
         .grid {
@@ -138,8 +136,7 @@ export default function Home() {
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 1rem;
+          max-width: 900px;
           flex-direction: row;
         }
 
@@ -170,10 +167,6 @@ export default function Home() {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
         }
 
         @media (max-width: 600px) {
@@ -260,6 +253,17 @@ export default function Home() {
           margin-top: -10px;
           text-shadow: 0px 16px 10px rgba(0, 0, 0, 0.3);
           transform: translate(0, -8);
+        }
+        .timer {
+          margin: 0 0 0.5rem 0;
+          line-height: 1.75;
+          font-size: 3vmax;
+          color: #0070f3;
+        }
+        .subtitle {
+          line-height: 1.5;
+          font-size: 2vmax;
+          color: #4867aa;
         }
       `}</style>
     </div>
