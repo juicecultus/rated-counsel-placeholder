843646d6a2450f5a31ef897211f95e69cf581a8e
juicecultus <justinian.boghici@gmail.com>
2021-12-13 16:25:31 +0000

    update next

--- components/Timer.js
+++ components/Timer.js
@@ -19,7 +19,6 @@ function Timer() {
   };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  // const [year] = useState(new Date().getFullYear());
 
   useEffect(() => {
     setTimeout(() => {
